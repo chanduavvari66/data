a6a08ff64b1a7d2f03d4f3e63c616bc864d58491
Chandu <chanduavvari66@gmail.com>
2023-05-27 17:54:27 +0530

    Updated LWC Comp
    Added currency app

--- force-app/main/default/Lwc with site/lwc/projectsResume/projectsResume.js
+++ force-app/main/default/Lwc with site/lwc/projectsResume/projectsResume.js
@@ -1,15 +1,20 @@
 import { LightningElement } from 'lwc';
 import bmi from '@salesforce/resourceUrl/Bmi_Image';
 import survey from '@salesforce/resourceUrl/Survey_Image';
+import currency from '@salesforce/resourceUrl/converter';
 export default class ProjectsResume extends LightningElement {
     BmiImage = bmi
     SurveyImage = survey
-
+    CurrencyImage = currency
 
     bmiLink(){
         window.open('https://chandu-portfolio-dev-ed.my.site.com/chandusite/bmi-calculator')
     }
     surveyLink(){
         window.open('https://chandu-portfolio-dev-ed.my.site.com/Esurvey')
+    }
+    currencyLink(){
+        window.open('https://chandu-portfolio-dev-ed.my.site.com/chandusite/currency-converter')
+
     }
 }
